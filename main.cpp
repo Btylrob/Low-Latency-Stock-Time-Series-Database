3cd7a92a714ae61ca20c4ca2d8a455dd47343b94
btylrob <1802brad@gmail.com>
2025-09-04 09:05:57 -0400

    Added tokenization of input

--- main.cpp
+++ main.cpp
@@ -1,24 +1,51 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <vector>
+
 #include "setUser.h"
 #include "loadTables.h"
 #include "help.h"
+#include "login.h"
+#include "csvParse.h"
+
+
+
+// TODO MAKE THIS ALL TOKENIZED AND TRY TO AVOID A FOREVER LONG IF CASE
 
 
 
 void print_prompt() {
-    std::cout << "db >";
+    std::cout <<  loggedUser + "db >";
 }
 
 int main() {
-    std::string input;
+
+
+std::array<std::string, 5> keywords = {"SELECT", "*", "FROM", "TEST"};
+
+
 
 
 
     while (true) {
         print_prompt();
-        std::cin >> input;
+        std::string input;
+        std::getline(std::cin, input);
+
+        std::stringstream ss(input);
+        std::vector<std::string> tokens;
+        std::string token;
+
+        while (ss >> token) {
+            tokens.push_back(token);
+        }
+
+        if (tokens.empty()) {
+            continue;
+        }
+
+        const std::string& command = tokens[0];
 
         if (input == "HELP") {
             help();
@@ -30,21 +57,66 @@ int main() {
             break;
         }
 
-        if (input == "SETUSR") {
-            setUser();
+        if (input == "MKUSR") {
+            mkUser();
             continue;
         }
 
-        if (input == "\\s") {
-            listSpaces();
+        if (input == "-V" || input == "-VERSION") {
+            std::cout << "1.0.0" << std::endl;
             continue;
         }
+
+        if (input == "GETUSR") {
+            getUserString();
+            continue;
+        }
+
+        if (input == "LOGIN") {
+            loginUser();
+            continue;
+        }
+
+
+        if (input == "TESTPARSE") {
+            testParse();
+            continue;
+        }
+
+
+
+
+
+        if (input == "\\s") {
+            //listSpaces();
+            continue;
+        }
+
+        bool containsKnownCommand = std::any_of(tokens.begin(), tokens.end(), [&](const std::string& t) {
+            return std::find(keywords.begin(), keywords.end(), t) != keywords.end();
+        });
+
+        if (containsKnownCommand) {
+            if (tokens.size() >= 3 &&
+                tokens[0] == "SELECT" &&
+                tokens[1] == "*" &&
+                tokens[2] == "FROM") {
+                std::cout << "COMMAND SUCCESSFULLY EXECUTED";
+
+                continue;
+            } else {
+                std::cout << "COMMAND NOT EXECUTED";
+                continue;
+            }
+        }
+
 
 
 
         std::cout << "unrecognized cmd"<< std::endl;
     }
 
+    return 0;
 }
 
 
